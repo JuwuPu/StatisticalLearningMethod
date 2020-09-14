12efce7759a0c5276a2c136ad7e3bff48dfc91eb
Johnny <juwupu@gmail.com>
2020-09-14 22:37:14 +0800

    The data structure of numpy drives me crazy.

--- MachineLearning/StatisticalLearningMethod/Perceptron/DualPerceptron.py
+++ MachineLearning/StatisticalLearningMethod/Perceptron/DualPerceptron.py
@@ -11,20 +11,28 @@ def Newtrain(train_set):
         count += 1
     return Newset
 
-def train(train_set,iteration_num, eta):
-    w = np.zeros((train_set.shape[1]-1, 1))
-    for j in range(iteration_num):
-        x = random.choice(train_set)
+def train(train_set, iteration_num, eta):
+    m, n = np.shape(train_set)
+    w = np.zeros((train_set.shape[1] - 1, 1))
+    RNS = np.random.randint(1, m+1, (iteration_num, 1))
+    i_count = 0
+    for i in RNS:
+        i_count += 1
+        x = train_set[int(i)-1]
         x = np.reshape(x, (1, train_set.shape[1]))
+        j_count = 0
         if x[0][0]*x[0, 1:].reshape((1, train_set.shape[1]-1))@w <= 0:
-            w += (eta*x[0][0]*x[0, 1:].reshape((1, train_set.shape[1]-1))).transpose()
+            for j in RNS[0:i_count, :].reshape((i_count,1)):
+                if int(j) == int(i):
+                    j_count += 1
+            w += (eta*j_count*x[0][0]*x[0, 1:].reshape((1, train_set.shape[1]-1))).transpose()
     return w
 
 def Visualization(train_set,w):
     plt.figure()
     x1 = np.linspace(0, 8, 100)
     x2 = (-w[-1]-w[0]*x1)/w[1]
-    plt.plot(x1, x2, color='r', label='OriginalPerceptron')
+    plt.plot(x1, x2, color='r', label='DualPerceptron')
     for i in range(train_set.shape[0]):
         if train_set[i][0] == 1:
             plt.scatter(train_set[i][1], train_set[i][2], s=100)
@@ -41,11 +49,4 @@ if __name__ == '__main__':
     weight = train(train_set=NewTrain_set, iteration_num=1000, eta=0.01)
     print(weight)
     Visualization(NewTrain_set, weight)
-
-
-
-
-
-
-
 
