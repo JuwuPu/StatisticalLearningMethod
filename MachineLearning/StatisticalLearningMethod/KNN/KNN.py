30b8ac7c6f6412845e0c1c9b8842634f2327e1fe
Johnny <juwupu@gmail.com>
2020-09-15 02:35:28 +0800

    def df2ndarray

--- MachineLearning/StatisticalLearningMethod/KNN/KNN.py
+++ MachineLearning/StatisticalLearningMethod/KNN/KNN.py
@@ -1,2 +1,24 @@
 import numpy as np
+import pandas as pd
+import random as rd
 import matplotlib.pyplot as plt
+
+def df2ndarray(data):
+    rows, columns = data.shape
+    data_matrix = np.zeros((rows-1, columns))
+    data = data.values
+    for i in range(rows-1):
+        for j in range(columns):
+            if j != 4:
+                data_matrix[i][j] = float(data[i+1][j])
+            elif data[i+1][j] == 'Iris-setosa':
+                data_matrix[i][j] = 0
+            elif data[i+1][j] == 'Iris-versicolor':
+                data_matrix[i][j] = 1
+            else:
+                data_matrix[i][j] = 2
+    return data_matrix
+
+if __name__ == '__main__':
+    data = pd.read_csv('IRIS.csv', header=None)
+    data_matrix = df2ndarray(data)
