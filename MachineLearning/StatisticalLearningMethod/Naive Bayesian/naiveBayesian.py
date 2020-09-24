dee3b0fd173cfb6ad8b58bc59babbb0aef25ecb5
JuwuPu <juwupu@gamil.com>
2020-09-25 03:26:19 +0800

    Using naive bayesian classifier on MNIST dataset.

--- MachineLearning/StatisticalLearningMethod/Naive Bayesian/naiveBayesian.py
+++ MachineLearning/StatisticalLearningMethod/Naive Bayesian/naiveBayesian.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-
+import time
+from collections import Counter
 
 def load_data(trainfile, testfile):
     train = pd.read_csv(trainfile, header=None)
@@ -11,17 +12,31 @@ def load_data(trainfile, testfile):
     test = test.drop(labels=test.columns[0], axis=1)
     return np.array(train), np.array(train_label), np.array(test), np.array(test_label)
 
-train, train_label, test, test_label = load_data(
-    'D:\PythonDocument'
-    '\MachineLearning'
-    '\StatisticalLearningMethod'
-    '\DataSources'
-    '\MNIST'
-    '\mnist_train.csv',
-    'D:\PythonDocument'
-    '\MachineLearning'
-    '\StatisticalLearningMethod'
-    '\DataSources'
-    '\MNIST'
-    '\mnist_test.csv'
-)
+def bayesianModel(train, train_label):
+    '''
+    This section contain two parts
+    Part 1 : prior probability
+    Part 2 : posterior probability
+    '''
+    # Part 1
+    totalNum = train.shape[0]
+    classNum = Counter(train_label)
+    prioriP = np.array([classNum[i]/totalNum for i in range(10)])
+
+    # Part 2
+    posteriorNum = np.zeros((10, train.shape[1]))
+    posteriorP = np.zeros((10, train.shape[1]))
+    for j in range(10):
+        posteriorNum[j] = train[np.where(train_label == j)].sum(axis=0)
+        # laplace smoothing
+        posteriorP[j] = (posteriorNum[j] + 1) / (classNum[j] + 2)
+    return prioriP, posteriorP
+
+def main():
+    train, train_label, test, test_label = load_data('mnist_train.csv', 'mnist_test.csv')
+
+if __name__ == '__main__':
+    start = time.time()
+    main()
+    end = time.time()
+    print('Time cost {0:.2f}s'.format(end - start))
