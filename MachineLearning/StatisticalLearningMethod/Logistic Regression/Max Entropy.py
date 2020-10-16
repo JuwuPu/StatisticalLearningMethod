fcaa4b0397eb67890d84b12eda7db8e49195ea71
JuwuPu <juwupu@gmail.com>
2020-10-17 01:09:51 +0800

    Training time is too long, indicating the maximum entropy model is not suitable for training large data.

--- MachineLearning/StatisticalLearningMethod/Logistic Regression/Max Entropy.py
+++ MachineLearning/StatisticalLearningMethod/Logistic Regression/Max Entropy.py
@@ -1,6 +1,8 @@
+import cv2
 import numpy as np
 import pandas as pd
 import time
+from collections import defaultdict
 
 
 def load_data(trainfile, testfile):
@@ -17,3 +19,68 @@ def load_data(trainfile, testfile):
     print('data is loaded')
     return np.array(train), np.array(train_label), np.array(test), np.array(test_label)
 
+def binaryzation(img):
+    cv_img = img.astype(np.uint8)
+    cv2.threshold(cv_img,50,1,cv2.THRESH_BINARY,cv_img)
+    return cv_img
+
+def binaryzation_features(trainset):
+    features = []
+    for img in trainset:
+        img = np.reshape(img,(28,28))
+        cv_img = img.astype(np.uint8)
+        img_b = binaryzation(cv_img)
+        features.append(img_b)
+
+    features = np.array(features)
+    features = np.reshape(features,(-1,784))
+    return features
+
+def rebuilt_features(features):
+    new_features = []
+    for feature in features:
+        new_feature = []
+        for i, f in enumerate(feature):
+            new_feature.append((str(i) + '_' + str(f)))
+        new_features.append(new_feature)
+    return new_features
+
+def IIS(train, train_label, iter=1000):
+    N = len(train)      # number of train sample
+
+    label = set(train_label)
+    Vxy = defaultdict(int)
+    for i in range(N):
+        x_, y = train[i], train_label[i]
+        for x in x_:
+            Vxy[(x, y)] += 1
+
+    id2xy = {}
+    xy2id = {}
+    for i, (x, y) in enumerate(Vxy):
+        id2xy[i] = (x, y)
+        xy2id[(x, y)] = i
+
+    Pxy = defaultdict(float)
+    for id in range(len(Vxy)):
+        (x, y) = id2xy[id]
+        Pxy[id] = float(Vxy[(x, y)]) / float(N)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+train, train_label, test, test_label = load_data('mnist_train.csv', 'mnist_test.csv')
+train = binaryzation_features(train)
+test = binaryzation_features(test)
+
+train = rebuilt_features(train)
